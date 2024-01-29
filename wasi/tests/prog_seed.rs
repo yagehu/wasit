7b0e328f02f2c25799dab9abc689819be87fc8f6
Yage Hu <me@huyage.dev>
2024-01-29 09:39:58 -0800

    Support fd_fdstat_set_flags

--- wasi/tests/prog_seed.rs
+++ wasi/tests/prog_seed.rs
@@ -686,3 +686,58 @@ fn fd_fdstat_get() {
         snapshot
     );
 }
+
+#[test]
+fn fdstat_set_flags() {
+    let path = wazzi_compile_time::root()
+        .join("seeds")
+        .join("11-fdstat_set_flags.json");
+    let f = fs::OpenOptions::new().read(true).open(&path).unwrap();
+    let seed: ProgSeed = serde_json::from_reader(f).unwrap();
+    let base_dir = tempdir().unwrap();
+    let wasmtime = wazzi_runners::Wasmtime::new("wasmtime");
+    let stderr = Arc::new(Mutex::new(Vec::new()));
+    let executor = Arc::new(
+        ExecutorRunner::new(wasmtime, executor_bin(), Some(base_dir.path().to_owned()))
+            .run(stderr.clone())
+            .expect("failed to run executor"),
+    );
+    let store = Arc::new(Mutex::new(InMemorySnapshotStore::default()));
+    let (tx, rx) = mpsc::channel();
+    let store_ = store.clone();
+    let mut executor_ = executor.clone();
+
+    thread::spawn(move || {
+        let spec = spec();
+        let mut store = store_.lock().unwrap();
+        let result = seed.execute(&mut executor_, &spec, store.deref_mut());
+
+        tx.send(result).unwrap();
+    });
+
+    let result = rx.recv_timeout(time::Duration::from_millis(250));
+
+    executor.kill();
+
+    let stderr = String::from_utf8(stderr.lock().unwrap().deref().clone()).unwrap();
+    let result = match result {
+        | Ok(result) => result,
+        | Err(err) => {
+            panic!("Execution timeout or error. stderr:\n{stderr}err:\n{err}")
+        },
+    };
+
+    assert!(result.is_ok(), "{:#?}\n{stderr}", result);
+
+    let store = store.lock().unwrap();
+    let snapshot = store
+        .get_snapshot(store.snapshot_count() - 1)
+        .unwrap()
+        .unwrap();
+
+    assert!(
+        matches!(snapshot.errno, Some(0)),
+        "{:#?}\nstderr:\n{stderr}\n",
+        snapshot
+    );
+}
