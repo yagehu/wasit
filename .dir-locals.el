fd5d895020bb7e428550465fa514c7f66f8e55a4
Yage Hu <me@huyage.dev>
2024-03-08 23:48:57 -0800

    Fix dir locals emacs config

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,5 @@
-((nil . ((eval . (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\runtimes\\'")
-                 (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\target\\'")
-                 (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\wasi-sdk\\'"))))
+((nil . ((eval . (add-to-list 'lsp-file-watch-ignored-directories
+                              "[/\\\\]\\runtimes\\'"
+                              "[/\\\\]\\target\\'"
+                              "[/\\\\]\\wasi-sdk\\'"))))
+
