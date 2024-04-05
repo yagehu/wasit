38b6a7b817eb24030d1360d1b0efd7bfedd65ec2
Yage Hu <me@huyage.dev>
2024-04-05 13:12:09 -0700

    fd_filestat_get

--- wazi/tests/seeds.rs
+++ wazi/tests/seeds.rs
@@ -234,6 +234,26 @@ fn main() -> Result<(), eyre::Error> {
                 Ok(())
             }),
         },
+        Case {
+            seed:   "13-fd_filestat_get.json",
+            spec:   "preview1.witx",
+            assert: Box::new(|run| {
+                let action = run
+                    .prog
+                    .store()
+                    .trace()
+                    .last_call()
+                    .unwrap()
+                    .wrap_err("no last call")?
+                    .read()
+                    .unwrap();
+                let call = action.call().unwrap();
+
+                assert_eq!(call.errno, Some(0));
+
+                Ok(())
+            }),
+        },
     ];
 
     for (i, case) in cases.into_iter().enumerate() {
