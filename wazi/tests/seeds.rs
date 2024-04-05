25e55dcc13ff61200de3766e7b8000bc433a9599
Yage Hu <me@huyage.dev>
2024-04-05 13:07:46 -0700

    fd_fdstat_set_flags

--- wazi/tests/seeds.rs
+++ wazi/tests/seeds.rs
@@ -194,6 +194,26 @@ fn main() -> Result<(), eyre::Error> {
                 Ok(())
             }),
         },
+        Case {
+            seed:   "11-fd_fdstat_set_flags.json",
+            spec:   "preview1.witx",
+            assert: Box::new(|run| {
+                let action = run
+                    .prog
+                    .store()
+                    .trace()
+                    .last_call()
+                    .unwrap()
+                    .wrap_err("no last call")?
+                    .read()
+                    .unwrap();
+                let call = action.call().unwrap();
+
+                assert_eq!(call.errno, Some(0));
+
+                Ok(())
+            }),
+        },
     ];
 
     for (i, case) in cases.into_iter().enumerate() {
