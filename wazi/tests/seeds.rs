10540846bbe69cedfc5699fd3f82cfce4eaad55e
Yage Hu <me@huyage.dev>
2024-04-05 13:09:04 -0700

    fd_fdstat_set_rights

--- wazi/tests/seeds.rs
+++ wazi/tests/seeds.rs
@@ -214,6 +214,26 @@ fn main() -> Result<(), eyre::Error> {
                 Ok(())
             }),
         },
+        Case {
+            seed:   "12-fd_fdstat_set_rights.json",
+            spec:   "preview1.witx",
+            assert: Box::new(|run| {
+                let action = run
+                    .prog
+                    .store()
+                    .trace()
+                    .last_call()
+                    .unwrap()
+                    .wrap_err("no last call")?
+                    .read()
+                    .unwrap();
+                let call = action.call().unwrap();
+
+                assert_eq!(call.errno, Some(0));
+
+                Ok(())
+            }),
+        },
     ];
 
     for (i, case) in cases.into_iter().enumerate() {
