a553617a26c1969c3ec594c5ce6403db93cad1f1
Yage Hu <me@huyage.dev>
2024-04-21 16:01:53 -0700

    Cleanup spec constraint evaluator

--- spec/constraint/src/lib.rs
+++ spec/constraint/src/lib.rs
@@ -4,9 +4,9 @@ pub use program::Program;
 
 use interval::IntervalSet;
 
-use crate::program::{ConstraintSet, TypeRef};
+use crate::program::ConstraintSet;
 
-pub fn evaluate(ctx: &Context, prog: &Program) -> ConstraintSet {
+pub fn evaluate(_ctx: &Context, prog: &Program) -> ConstraintSet {
     let cset = prog.expr.eval();
 
     cset
@@ -28,11 +28,8 @@ pub struct Constraint {
 
 #[cfg(test)]
 mod tests {
-    use arbitrary::Unstructured;
-    use gcollections::ops::{Bounded, Cardinality};
     use interval::interval_set::ToIntervalSet;
     use num_bigint::BigInt;
-    use rand::{thread_rng, RngCore};
 
     use crate::{
         evaluate,
@@ -74,43 +71,5 @@ mod tests {
         });
 
         assert_eq!(iset, vec![(1, 1000)].to_interval_set());
-    }
-
-    #[test]
-    fn ok() {
-        let mut rng = thread_rng();
-        let mut data = vec![0u8; 64];
-
-        rng.fill_bytes(&mut data);
-
-        let iset0 = vec![(1u64, 1), (10, 19), (30, 100)].to_interval_set();
-        let iset1 = vec![(1, 10), (10, 20)].to_interval_set();
-
-        assert_eq!(iset0.interval_count(), 3);
-        assert_eq!(iset1.interval_count(), 1);
-
-        let mut u = Unstructured::new(&data);
-        let is0 = iset0.iter().collect::<Vec<_>>();
-        let size: usize = is0.into_iter().map(|int| int.size() as usize).sum();
-
-        eprintln!("{size}");
-
-        for _ in 0..30 {
-            let mut i = u.choose_index(size).unwrap() as u64;
-            let mut value = 0;
-
-            for int in iset0.iter() {
-                let size = int.size();
-
-                if size > i {
-                    value = int.lower() + i;
-                    break;
-                }
-
-                i -= size;
-            }
-
-            eprintln!("value {value}");
-        }
     }
 }
